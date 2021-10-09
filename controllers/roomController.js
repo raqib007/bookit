e47d6ccf0e10554cbe39e84db21808889f54e4b5
raqib007 <rhsabbir@yahoo.com>
2021-10-09 19:15:49 +0600

    third commit

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,15 +1,31 @@
 import Room from '../models/room';
 import ErrorHandler from "../utils/errorHandler";
 import catchAsyncErrors from "../middlewares/catchAsyncErrors";
+import APIFeatures from "../utils/apiFeatures";
 
 //get all rooms => /api/rooms
 const allRooms = catchAsyncErrors(async (req, res) => {
-    const rooms = await Room.find();
+
+    const resPerPage = 4;
+    const roomsCount = await Room.countDocuments();
+
+    const apiFeatures = new APIFeatures(Room.find(),req.query)
+        .search()
+        .filter();
+
+    let rooms = await apiFeatures.query;
+
+    let filteredRoomsCount = rooms.length;
+
+    apiFeatures.pagination(resPerPage);
+    rooms = await apiFeatures.query.clone();
 
     res.status(200).json({
         success: true,
-        count: rooms.length,
-        rooms: rooms
+        roomsCount,
+        resPerPage,
+        filteredRoomsCount,
+        rooms
     })
 
 })
